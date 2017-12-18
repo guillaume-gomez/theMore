9b8d09473c63f9707175d8ac0b4fa7c0245d02f2
guillaume-gomez <guillaume.gomez846@gmail.com>
2017-12-19 00:51:05 +0100

    slightly improve the enemy script

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -5,13 +5,14 @@ using Random = UnityEngine.Random;
 
 public class Enemy : MonoBehaviour {
 
-    public float minTarX = 1f;
+    public float minTarX = -10f;
     public float maxTarX = 10f;
-    public float minTarY = 1f;
+    public float minTarY = -10f;
     public float maxTarY = 10f;
     public float speed = 0.8f;
+    public LayerMask blockingLayer;
 
-    public Vector3 target;
+    private Vector3 target;
 
     void Start() {
         CreateTarPoint();
@@ -24,15 +25,24 @@ public class Enemy : MonoBehaviour {
         float tarX = Random.Range(minTarX, maxTarX) - dampX;
         float tarY = Random.Range(minTarY, maxTarY) - dampY;
         target = new Vector3(tarX + transform.position.x, tarY + transform.position.y, 0.0f);
+        Debug.Log(tarX + " " + tarY);
     }
 
     void Update() {
         if(Vector3.Distance(transform.position, target) <= 0.5) {
             CreateTarPoint();
-        } else {
+            return;
+        }
+
+        RaycastHit2D hit;
+        hit = Physics2D.Linecast(transform.position, target, blockingLayer);
+        if(hit.transform == null) {
             float step = speed * Time.deltaTime;
             transform.position = Vector3.MoveTowards(transform.position, target, step);
+        } else {
+            target = new Vector3(-target.x, -target.y, 0f);
         }
+
     }
 
 }
