3715d3097ee8ffc8663ed7c006341b0530e45e6b
guillaume-gomez <guillaume.gomez846@gmail.com>
2018-01-08 00:21:52 +0200

    fix where enemy shoots

--- Assets/Scripts/BulletEnemy.cs
+++ Assets/Scripts/BulletEnemy.cs
@@ -3,20 +3,24 @@ using System.Collections;
 
 public class BulletEnemy : MonoBehaviour
 {
-    public float speed = 15.0f;
+    public float speed = 20.0f;
     private Rigidbody2D rb2D;
     private GameObject player;
 
     void Start() {
         player = GameObject.FindWithTag("Player");
-        rb2D = GetComponent<Rigidbody2D>();
-        Vector2 target = Camera.main.ScreenToWorldPoint( new Vector2(player.transform.position.x, player.transform.position.y) );
-        Vector2 myPos = new Vector2(transform.position.x, transform.position.y);
-        Vector2 direction = target - myPos;
-        direction.Normalize();
-
-        rb2D.velocity = direction * speed;
-        Destroy(gameObject, 2.0f);
+        if(player) {
+            rb2D = GetComponent<Rigidbody2D>();
+            Vector2 target = new Vector2(player.transform.position.x, player.transform.position.y);
+            Vector2 myPos = new Vector2(transform.position.x, transform.position.y);
+            Vector2 direction = target - myPos;
+            direction.Normalize();
+            rb2D.velocity = direction * speed;
+            Destroy(gameObject, 2.0f);
+        } else {
+            // dont need to keep the bullet alive
+            Destroy(gameObject);
+        }
     }
 
 }
