509e2b1b7f1a85b9924529dcbda883510cc5b0f9
guillaume-gomez <guillaume.gomez846@gmail.com>
2018-02-10 00:05:49 +0100

    set button dynamically

--- Assets/Scripts/TransitionMenuScript.cs
+++ Assets/Scripts/TransitionMenuScript.cs
@@ -5,10 +5,28 @@ using UnityEngine.SceneManagement;
 
 public class TransitionMenuScript : MonoBehaviour
 {
-    private float restardLevelDelay = 2f;
+    private float restardLevelDelay = 200f; // temporary
+    private float delayShowText = 5f;
+
+    public GameObject textCanvas;
+    public GameObject weaponSelectionCanvas;
 
     void Start() {
-        Invoke("LoadingLevel", restardLevelDelay);
+        //ShowTextCanvas();
+        ShowWeaponSelection();
+        //Invoke("LoadingLevel", restardLevelDelay);
+    }
+
+    private void ShowTextCanvas() {
+      textCanvas.SetActive(true);
+      weaponSelectionCanvas.SetActive(false);
+      Invoke("ShowWeaponSelection", delayShowText);
+    }
+
+    private void ShowWeaponSelection() {
+      textCanvas.SetActive(false);
+      weaponSelectionCanvas.SetActive(true);
+      Invoke("LoadingLevel", restardLevelDelay);
     }
 
     private void LoadingLevel()
