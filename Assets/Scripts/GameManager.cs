41095e0691206d55d656a471165d7a055949f141
guillaume-gomez <guillaume.gomez846@gmail.com>
2018-03-01 21:36:41 +0100

    update game manager to manage levels

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -2,17 +2,27 @@ using UnityEngine;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour
 {
     public static GameManager instance = null;
     public BoardManager boardScript;
 
+    public string GameOverDie = "you died";
+    public string GameOverKillInnocent = "you killed an honest citizen";
+
     private int level = 1;
     private bool needUpdate;
-    private float restardLevelDelay = 1f;
+    private float restardLevelDelay = 2f;
     private int nbWeaponsAvailable = 1;
     private int nbLevelBeforeNextWeapon = 5;
+    private int currentIndexWeaponHero = 0;
+    private GameObject gameOverImage;
+    private Text gameOverText;
+    private GameObject restardButton;
+
+    private bool doingSetup;
 
     public int NbWeaponsAvailable {
         get
@@ -23,6 +33,26 @@ public class GameManager : MonoBehaviour
             return instance.nbWeaponsAvailable;
         }
     }
+
+    public int NbLevelBeforeNextWeapon {
+        get
+        {
+            if(instance == null) {
+                instance = this;
+            }
+            return instance.nbLevelBeforeNextWeapon;
+        }
+    }
+    public int Level {
+        get {
+            if(instance == null) {
+                instance = this;
+            }
+            return instance.level;
+        }
+    }
+
+    public int CurrentIndexWeaponHero { get; set; }
 
     void Awake()
     {
@@ -56,19 +86,41 @@ public class GameManager : MonoBehaviour
     void InitGame()
     {
         Debug.Log("InitGame level: " + level);
+        gameOverImage = GameObject.Find("GameOverImage");
+        gameOverText = GameObject.Find("GameOverText").GetComponent<Text>();
+
+        GameObject.Find("RestartButton").GetComponent<Button>().onClick.AddListener(() => OnRestartClick());
+        gameOverImage.SetActive(false);
+
         needUpdate = true;
-        boardScript.SetupScene(level);
+        int size =  50 + (level % nbLevelBeforeNextWeapon) * 10;
+        boardScript.SetupScene(level, size, size);
     }
 
-    public void GameOver()
+    private void OnRestartClick() {
+        level = 1;
+        ReloadCurrentLevel();
+    }
+
+    public void GameOver(string reason)
     {
-        // DO SOME STUFF
-        Invoke("ReloadCurrentLevel", restardLevelDelay);
-        enabled = false;
+        gameOverText.text = "After " + level + " days, " + reason + ".";
+        gameOverImage.SetActive(true);
+
     }
 
     public void NextLevel() {
         if(needUpdate) {
+            GameObject[] enemyBullets = GameObject.FindGameObjectsWithTag("BulletEnemy");
+            for(int i = 0; i < enemyBullets.Length; ++i) {
+                Destroy(enemyBullets[i]);
+            }
+            GameObject[] enemys = GameObject.FindGameObjectsWithTag("Enemy");
+            for(int i = 0; i < enemys.Length; ++i) {
+                Debug.Log("forbidden");
+                //Destroy(enemys[i]);
+                enemys[i].GetComponent<Enemy>().HasWeapon = false;
+            }
             level++;
             if(level % nbLevelBeforeNextWeapon == 0) {
                 nbWeaponsAvailable++;
@@ -84,6 +136,8 @@ public class GameManager : MonoBehaviour
     }
 
     private void ReloadCurrentLevel() {
+        gameOverImage.SetActive(true);
+
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex, LoadSceneMode.Single);
     }
 
